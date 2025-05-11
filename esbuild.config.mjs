135649dfe54100576835e16efc4344ac4727168d
memodack <obsidian-memodack-plugin@proton.me>
2025-05-11 16:38:31 +0300

    chore: update esbuild.config.mjs
    - add `styles.css` watcher

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,11 +6,13 @@ import { context } from 'esbuild';
 import { existsSync } from 'node:fs';
 import { exit } from 'node:process';
 import { join } from 'node:path';
+import { watchFile } from 'fs';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isProduction = process.env.NODE_ENV === 'production';
 
-const outdir = 'dist';
+const outdirPath = 'dist';
+const stylesCssFilePath = 'src/styles.css';
 
 const external = [
   'obsidian',
@@ -29,18 +31,26 @@ const external = [
   ...builtins,
 ];
 
-if (existsSync(outdir)) {
-  await rm(outdir, { recursive: true, force: true });
+if (existsSync(outdirPath)) {
+  await rm(outdirPath, { recursive: true, force: true });
 }
+
+const stylesCopy = async () => {
+  const css = await readFile(stylesCssFilePath, 'utf8');
+  const minified = new CleanCSS().minify(css).styles;
+
+  await writeFile(join(outdirPath, 'styles.css'), minified);
+};
+
+watchFile(stylesCssFilePath, { interval: 300 }, async () => {
+  await stylesCopy();
+});
 
 const stylesCopyPlugin = () => ({
   name: 'styles-copy-plugin',
   setup(build) {
     build.onEnd(async () => {
-      const css = await readFile('src/styles.css', 'utf8');
-      const minified = new CleanCSS().minify(css).styles;
-
-      await writeFile(join(outdir, 'styles.css'), minified);
+      await stylesCopy();
     });
   },
 });
@@ -49,14 +59,14 @@ const manifestCopyPlugin = () => ({
   name: 'manifest-copy-plugin',
   setup(build) {
     build.onEnd(async () => {
-      await copyFile('manifest.json', join(outdir, 'manifest.json'));
+      await copyFile('manifest.json', join(outdirPath, 'manifest.json'));
     });
   },
 });
 
 const ctx = await context({
   entryPoints: ['src/main.ts'],
-  outfile: `${outdir}/main.js`,
+  outfile: `${outdirPath}/main.js`,
   bundle: true,
   minify: isProduction,
   format: 'cjs',
